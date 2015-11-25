3abc3eca91cf65b298a69e0da025e6b68a862f72
Thanh Pham <minhthanh@me.com>
2015-11-25 21:04:46 +0800

    Initial commit

---
+++ Fish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fish
+//
+//  Created by Thanh Pham on 11/25/15.
+//  Copyright © 2015 Thanh Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fish
+//
+//  Created by Thanh Pham on 11/25/15.
+//  Copyright © 2015 Thanh Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
